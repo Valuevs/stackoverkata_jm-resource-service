131ae6d5e6f0cda8a703dcc9d7bf04a1d19ef5c8
Musheg Minasyan <musho.arm@mail.ru>
2024-11-01 13:57:45 +0000

    Resolve "Реализовать api, которое позволяет добавить комментарий к ответу на вопрос #24"v1

---
+++ src/main/java/stackover/resource/service/service/dto/impl/UserService.java
@@ -0,0 +1,22 @@
+package stackover.resource.service.service.dto.impl;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import stackover.resource.service.entity.user.User;
+import stackover.resource.service.repository.entity.QuestionsRepository;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+
+
+    private final QuestionsRepository.UserRepository userRepository;
+
+    public Optional<User> findById(Long id) {
+        // Заглушка: возвращаем любого пользователя из базы данных
+        // TODO: Реализовать поиск пользователя по id
+        return userRepository.findById(id);
+    }
+}
